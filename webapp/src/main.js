b2aaedfa3f399c19269462720bdb3e615f373212
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-05-09 01:00:07 +0200

    Hierarchy example + initial config

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueTree from '@ssthouse/vue-tree-chart'
 
 Vue.config.productionTip = false
+
+Vue.component('vue-tree', VueTree)
 
 new Vue({
   router,
---
+++ webapp/src/views/Hierarchy.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <vue-tree
+        style="width: 800px; height: 600px; border: 1px solid gray;"
+        :dataset="sampleData"
+        :config="treeConfig"
+    >
+      <template v-slot:node="{ node, collapsed }">
+        <span
+            class="tree-node"
+            :style="{ border: collapsed ? '2px solid grey' : '' }"
+        >{{ node.value }}</span
+        >
+      </template>
+    </vue-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hierarchy',
+  data() {
+    return {
+      sampleData: {
+        value: '1',
+        children: [
+          { value: 'Manele', children: [{ value: '4' }, { value: '5' }] },
+          { value: '3' }
+        ]
+      },
+      treeConfig: { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 }
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.container
+  display: flex
+  flex-direction: column
+  align-items: center
+
+
+.tree-node
+  display: inline-block
+  width: 28px
+  height: 28px
+  border-radius: 50%
+  background-color: antiquewhite
+  text-align: center
+  line-height: 28px
+</style>
