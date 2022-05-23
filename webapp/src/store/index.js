0fe31c9c7157a98f99c09639fd493a49ea97c37b
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-05-23 23:54:48 +0200

    Add example API

--- webapp/src/store/index.js
+++ webapp/src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {Octokit} from "octokit";
+
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
         clusters: null,
-        labels: {}
+        labels: {},
+        searchData: null
     },
     mutations: {
         addClusters(state, data) {
@@ -15,11 +19,14 @@ export default new Vuex.Store({
         addLabels(state, data) {
             state.labels = data
         },
-        addLabel(state,data){
+        addLabel(state, data) {
             state.labels[data.id]['label'] = data.label
         },
-        addWeight(state, data){
+        addWeight(state, data) {
             state.labels[data.id]['weight'] = data.weight
+        },
+        storeSearchData(state, data) {
+            state.searchData = data
         }
     },
     actions: {
@@ -29,17 +36,37 @@ export default new Vuex.Store({
         storeLabels({commit}, data) {
             commit('addLabels', data)
         },
-        modifyLabel({commit}, data){
+        modifyLabel({commit}, data) {
             commit('addLabel', data)
+        },
+        async testOctokit({commit}) {
+
+            const octokit = new Octokit({
+                auth: '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'
+            })
+
+            const res = await octokit.request('GET /search/topics', {q : 'ruby+is:featured'})
+
+            commit('storeSearchData', res)
+
+
+
         }
+
+
     },
     getters: {
         getCluster(state) {
             return state.clusters
-        },
+        }
+        ,
         getLabels(state) {
             return state.labels
+        },
+        getSearchData(state) {
+            return state.searchData
         }
-    },
+    }
+    ,
     modules: {}
 })
--- webapp/src/views/Search.vue
+++ webapp/src/views/Search.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    SEARCH
-  </div>
+  <v-container>
+
+    <v-row justify="center" class="ma-4">
+      <v-col cols="2" class="justify-center text-center">
+        <v-btn class="text-center" rounded x-large color="black"><span class="white--text" @click="testOctokit"> TEST </span></v-btn>
+      </v-col>
+    </v-row>
+
+    <v-card shaped>
+      <v-card-title>Test Result</v-card-title>
+      <v-card-text>
+        <v-list rounded>
+          <v-list-item-group>
+            <v-list-item v-for="(res,index) in searchData" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ res.name }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ res.short_description }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 
+import {mapActions, mapGetters} from "vuex";
+
 export default {
   name: 'Search',
   components: {},
+  methods: {
+    ...mapActions(['testOctokit'])
+  },
+  computed: {
+    ...mapGetters(['getSearchData']),
+    searchData() {
+      return this.getSearchData ? this.getSearchData.data.items ? this.getSearchData.data.items : [] : []
+    }
+  }
 }
 </script>
